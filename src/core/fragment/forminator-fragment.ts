68b17afd583c34195c3a7878f673d22811f79fbf
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-07-07 18:40:16 +0430

    chore: add fragment, value, state concepts

---
+++ src/core/fragment/forminator-fragment.ts
@@ -0,0 +1,13 @@
+import { ReadonlyWire, Wire } from '@forminator/react-wire';
+import { StateWire } from '../state-composer/state-composer';
+import { Option } from '../../utils/option';
+import { ValueComposer } from '../value-composer/value-composer';
+
+export interface ForminatorFragment<IValue, EValue> {
+  readonly id: string;
+  composer$: Wire<Option<ValueComposer<IValue, EValue>>>;
+  value$: Wire<IValue | undefined>;
+  finalValue$$: Wire<Option<ReadonlyWire<Option<EValue>>>>;
+  stateWires$: Wire<Partial<Record<string, StateWire<any>>>>;
+  finalStateWires$: Wire<Partial<Record<string, ReadonlyWire<any, any>>>>;
+}
