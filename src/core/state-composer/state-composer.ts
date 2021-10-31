bcf75b8911a6f702a184c8e8c367742c93d48635
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-10-31 20:41:53 +0330

    fixup! chore: add fragment, value, state concepts

--- src/core/state-composer/state-composer.ts
+++ src/core/state-composer/state-composer.ts
@@ -1,19 +1,21 @@
 import { ReadonlyWire } from '@forminator/react-wire';
 import { Option } from '../../utils/option';
 
-export interface StateDefinition<State, Fns, Args extends any[]> {
+export interface StateDefinition<FinalState, State, Fns, Args extends any[]> {
+  finalState: FinalState;
   state: State;
   fns: Fns;
   args: Args;
 }
 
-export type StateWire<SD extends StateDefinition<any, any, any>> = ReadonlyWire<
-  SD['state'],
-  SD['fns']
->;
+export type StateWire<SD extends StateDefinition<any, any, any, any>> =
+  ReadonlyWire<SD['state'], SD['fns']>;
 
-export interface StateComposer<SD extends StateDefinition<any, any, any>> {
-  compose(state: Option<SD['state']>, states: SD['state'][]): SD['state'];
+export interface StateComposer<SD extends StateDefinition<any, any, any, any>> {
+  compose(
+    state: Option<SD['state']>,
+    states: SD['finalState'][],
+  ): SD['finalState'];
   create(...args: SD['args']): StateWire<SD>;
   getKey(): string;
 }
