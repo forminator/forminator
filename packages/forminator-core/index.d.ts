207b709d549c7a5fc55cede8b25b85928c52190a
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2022-01-11 19:20:57 +0330

    chore(core): add option type

--- packages/forminator-core/index.d.ts
+++ packages/forminator-core/index.d.ts
@@ -1,3 +1,37 @@
 export declare function createId(prefix?: string): string;
 
+export declare function fromOption<Value>(
+  option: Option_2<Value>,
+): Value | undefined;
+
+export declare function intoOption<Value>(
+  value: Value | undefined,
+): Option_2<Value>;
+
+export declare type None<Value> = {
+  some: false;
+} & OptionFns<Value>;
+
+export declare function none<Value>(): None<Value>;
+
+declare type Option_2<Value> = Some<Value> | None<Value>;
+export { Option_2 as Option };
+
+export declare interface OptionFns<Value> {
+  ok(this: Option_2<Value>): Value;
+  or(this: Option_2<Value>, value: Value): Value;
+  map<T>(this: Option_2<Value>, fn: (value: Value) => T): Option_2<T>;
+  isSome(this: Option_2<Value>): this is Some<Value>;
+  isNone(this: Option_2<Value>): this is None<Value>;
+}
+
+export declare type Some<Value> = {
+  some: true;
+  value: Value;
+} & OptionFns<Value>;
+
+export declare function some<Value>(
+  value: Value,
+): Some<Value> & OptionFns<Value>;
+
 export {};
--- packages/forminator-core/src/index.ts
+++ packages/forminator-core/src/index.ts
@@ -1,2 +1,5 @@
 /* utils */
 export { createId } from './utils/id';
+
+export { some, none, intoOption, fromOption } from './utils/option';
+export type { Option, OptionFns, Some, None } from './utils/option';
