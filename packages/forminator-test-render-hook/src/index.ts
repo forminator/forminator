842adfb01869a03d8a71446a8fca9b93af0a07d9
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2022-05-28 15:48:22 +0430

    feat(test-render-hook): catch errors and set in result ref

--- packages/forminator-test-render-hook/src/index.ts
+++ packages/forminator-test-render-hook/src/index.ts
@@ -1,7 +1,10 @@
 export { renderHook, act } from './render-hook';
 export type {
+  ResultRef,
   RenderHookResult,
   RenderHookOptions,
   RenderHookResultWithoutProps,
   RenderHookOptionsWithoutProps,
 } from './render-hook';
+
+export { suppressErrorOutput } from './suppress-error-output';
