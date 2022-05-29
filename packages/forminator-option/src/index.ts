ea8eee1012151a439912c829be360d27ca2d60f0
Seyyed Morteza Moosavi <smmoosavi@users.noreply.github.com>
2022-05-29 20:07:08 +0430

    feat(option): add @forminator/option package (#13)
    * feat(option): add @forminator/option package
    
    * feat(option): add some, none, intoOption, fromOption and types

---
+++ packages/forminator-option/src/index.ts
@@ -0,0 +1,11 @@
+export {
+  some,
+  none,
+  intoOption,
+  fromOption,
+  catchNoneError,
+  throwNoneError,
+  isNoneError,
+  NoneError,
+} from './option';
+export type { Option, OptionFns, Some, None } from './option';
