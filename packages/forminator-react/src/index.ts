66e91bd994e2101afaeff3f72c9df797a3c27a0e
Seyyed Morteza Moosavi <smmoosavi@users.noreply.github.com>
2022-10-02 15:55:06 +0330

    fix(react): export fragment and external value context provider, export dict dispatch and actions (#32)

--- packages/forminator-react/src/index.ts
+++ packages/forminator-react/src/index.ts
@@ -1,14 +1,20 @@
+export { ExternalValueContextProvider } from './contexts/external-value-context';
+export { FragmentContextProvider } from './contexts/fragment-context';
 export { type ForminatorProps, Forminator } from './forminator';
 export { useFragment } from './use-fragment';
 export { useRootFragment } from './use-root-fragment';
 export { useExternalValue } from './use-external-value';
 export { useComposer } from './use-composer';
 export { useInputValue$ } from './input/use-input-value';
+
 export { type DictInputProps, DictInput } from './dict-input/dict-input';
 export {
   type DictInputItemProps,
   DictInputItem,
 } from './dict-input/dict-input-item';
+export { useDictInputDispatchContext } from './dict-input/DictInputDispatchContext';
+export { addField, removeField } from './dict-input/reducer';
+
 export { useArrayIndexContext } from './array-input/ArrayIndexContext';
 export { useArrayInputDispatchContext } from './array-input/ArrayInputDispatchContext';
 export { type ArrayInputProps, ArrayInput } from './array-input/array-input';
